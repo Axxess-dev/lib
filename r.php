df3e5f501adf9457b7c1c3c5a55c751dfe18bd96
Axxess Developer <57417429+Axxess-dev@users.noreply.github.com>
2020-07-16 15:43:20 -0500

    Update r.php

--- r.php
+++ r.php
@@ -1,3 +1,12 @@
-<?php
-
-?>
+<?php class R {
+  function __construct($r=false){
+    foreach($r as $k){
+      $f=$k.'.php';
+      if(file_exists($f)){
+        require_once($f);
+      }else{
+        file_put_contents($f,file_get_contents('https://raw.githubusercontent.com/Axxess-dev/lib/master/'.$f));
+      }
+    }
+  }
+} ?>
